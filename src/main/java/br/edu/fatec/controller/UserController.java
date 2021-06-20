f6ba7bc6a728a07a85abded230fee46faecbacd8
Kleber Nascimento <kleber.nascimento5@fatec.sp.gov.br>
2021-06-19 23:01:57 -0300

    Cadastro de Usuários inserção

--- src/main/java/br/edu/fatec/controller/UserController.java
+++ src/main/java/br/edu/fatec/controller/UserController.java
@@ -1,21 +1,36 @@
 package br.edu.fatec.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.edu.fatec.model.Usuario;
+import br.edu.fatec.service.CadastroUsuarioService;
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
 	
+	@Autowired
+	private CadastroUsuarioService service;
+	
 	@GetMapping("/cadastrar")
-	public String cadastrar() {
+	public String cadastrar(Usuario usuario) {
 		return "/user/cadastro";
 	}
 	
 	@GetMapping("/listar")
 	public String listar() {
 		return "/user/lista";
+	}
+	
+	@PostMapping("/salvar")
+	public String salvar(Usuario usuario) {
+		service.salvar(usuario);
+		return "redirect:/users/cadastrar";
+		
 	}
 
 }
