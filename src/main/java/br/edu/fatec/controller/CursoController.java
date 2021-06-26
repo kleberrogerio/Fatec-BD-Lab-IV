a7cfd767461c1a4079ea0a73c1248ba4f3d9149d
Kleber Nascimento <kleber.nascimento5@fatec.sp.gov.br>
2021-06-26 14:21:50 -0300

    Cadastro e listagem de Cursos

--- src/main/java/br/edu/fatec/controller/CursoController.java
+++ src/main/java/br/edu/fatec/controller/CursoController.java
@@ -1,21 +1,39 @@
 package br.edu.fatec.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.edu.fatec.model.Curso;
+import br.edu.fatec.service.CadastroCursoService;
 
 @Controller
 @RequestMapping("/cursos")
 public class CursoController {
 	
+	@Autowired
+	private CadastroCursoService service;	
+	
 	@GetMapping("/cadastrar")
-	public String cadastrar() {
+	public String cadastrar(Curso curso) {
 		return "/curso/cadastro";
 	}
 	
 	@GetMapping("/listar")
-	public String listar() {
+	public String listar(ModelMap model) {
+		model.addAttribute("cursos",service.Listar());
 		return "/curso/lista";
 	}
+	
+	@PostMapping("/salvar")
+	public String salvar(Curso curso) {
+		service.salvar(curso);
+		return "redirect:/cursos/cadastrar";
+	}
+	
 
-}
+} 
