9e43aba3404c959918c70b857e5a2c192ed40161
Kleber Nascimento <kleber.nascimento5@fatec.sp.gov.br>
2021-06-26 16:40:09 -0300

    Iniciando a exclusão de Turmas

--- src/main/java/br/edu/fatec/controller/TurmaController.java
+++ src/main/java/br/edu/fatec/controller/TurmaController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -30,7 +31,13 @@ public class TurmaController {
 	@PostMapping("/salvar")
 	public String salvar(Turma turma) {
 		service.salvar(turma);
-		return "redirect:/turmas/cadastrar";
+		return "redirect:/turmas/cadastrar";		
+	}
+	
+	@GetMapping("/excluir/{id}")
+	public String excluir(@PathVariable("id") Long id, ModelMap model) {
+		service.excluir(id);
+		return listar(model);
 		
 	}
 
