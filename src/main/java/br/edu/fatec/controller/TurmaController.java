2e57db25e489830386cf1d53223507118ae6e6a0
Kleber Nascimento <kleber.nascimento5@fatec.sp.gov.br>
2021-06-28 23:48:26 -0300

    Editando as Turmas

--- src/main/java/br/edu/fatec/controller/TurmaController.java
+++ src/main/java/br/edu/fatec/controller/TurmaController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -32,6 +33,18 @@ public class TurmaController {
 		service.salvar(turma);
 		return "redirect:/turmas/cadastrar";
 		
+	}
+	
+	@GetMapping("/editar/{id}")
+	public String preEditar(@PathVariable("id") Long id, ModelMap model) {
+		model.addAttribute("turma",service.buscarPorId(id));
+		return "/turma/cadastro";
+	}
+	
+	@PostMapping("/editar")
+	public String editar(Turma turma) {
+		service.editar(turma);
+		return "redirect:/turmas/cadastrar";
 	}
 
 }
